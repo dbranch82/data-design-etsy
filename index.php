a5b9f726099831f94f45f8642c2f81fceea88d23
Danielle Branch <dbranch82@gmail.com>
2017-07-14 09:41:39 -0600

    Created project image path correct

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>About Mike</title>
+	</head>
+	<body>
+		<h1 id="page header">Meet Mike Waters</h1>
+	<img src="epic/image/mikefidgetspinner.jpg">
+		<h1>Persona</h1>
+		<p><strong>Name:</strong> Mike </p>
+		<p><strong>Age:</strong> 28</p>
+		<p><strong>Profession :</strong> Mike makes customized fidget spinners in his spare time. He is an engineer full time.</p>
+		<p><strong>Technology:</strong> Uses Mac book pro when he is dealing with business or at work and a newer Iphone for
+			daily use.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Mike is ready to take his small hobby and make it into a revenue generator. He
+		will be dedicating weekends and evenings to his fidget spinners. If it takes off he would be willing to quit his job and
+		customize spinners full time.</p>
+		<p><strong>Frustrations and Needs:</strong> Mike is looking for a place where he can display his past work and also give new
+		customers a place to pick different options. He does not want a personal website to sell his product on.</p>
+		<p><strong>Goals:</strong> Mike is looking to make money whilst doing something he enjoys. </p>
+		<p><strong>User Story:</strong> As a user, I need to see if this site will be the appropriate place to sell my goods
+		and if it will allow me to give my customers a unique experience.</p>
+
+		<h2><strong>Use Case</strong></h2>
+		<p>Mike has been looking at different websites where people can sell handmade goods.</p>
+		<p>Mikes biggest sticking point is giving his customers the option of customization and also being able to show his past work.</p>
+		<p>It is Thursday and Mike needs to be able to get his site up in under 30 minutes during his lunch break as he already
+		has people wanting to order. The weekend is fast approaching.</p>
+	</body>
+</html>
