759670949a01c56b4d9409947eb8f93555b4c541
dbranch6 <dbranch6@cnm.edu>
2017-07-18 16:33:32 -0600

    Finished and ran SQL

--- epic/sql/.sql
+++ epic/sql/.sql
@@ -1,19 +1,25 @@
-DROP TABLE IF EXISTS profile;
 DROP TABLE IF EXISTS item;
-
-CREATE TABLE item(
-itemId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	itemName VARCHAR(128)NOT NULL,
-	itemPrice VARCHAR(32)NOT NULL,
-itemProfileId INT UNSIGNED NOT NULL
-);
+DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile(
 profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-profileEmail VARCHAR(128)NOT NULL ,
+	profileEmail VARCHAR(128)NOT NULL ,
 	profilePhone VARCHAR(32),
 	profileHash CHAR(128)NOT NULL,
-	profileSalt CHAR(64)NOT NULL
-
-
+	profileSalt CHAR(64)NOT NULL,
+	UNIQUE (profileEmail),
+	UNIQUE (profilePhone),
+	PRIMARY KEY (profileId)
 );
+
+
+
+CREATE TABLE item(
+		itemProfileId INT UNSIGNED NOT NULL,
+		itemId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+		itemName VARCHAR (128) NOT NULL,
+		itemPrice VARCHAR (32) NOT NULL,
+	PRIMARY KEY (itemId),
+	FOREIGN KEY (itemprofileId) REFERENCES profile(profileId),
+	UNIQUE (itemName),
+	UNIQUE (itemPrice));
